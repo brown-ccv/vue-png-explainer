09000dc69147cb2d35baafb09162e76a9aa99138
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-07-17 10:57:15 -0400

    refactor: change expandable to details/summary tags

--- src/components/Expandable.vue
+++ src/components/Expandable.vue
@@ -1,6 +1,6 @@
 <template>
-  <li class="expandable mx-2 my-2">
-    <header
+  <details class="expandable mx-2 my-2">
+    <summary
       class="level is-mobile expandable-header mb-0 px-3 py-2"
       v-bind:class="classObject"
       @click="onSelect"
@@ -10,22 +10,14 @@
       @focus="setActive(true)"
       @blur="setActive(false)"
       tabindex="0"
-      role="button"
-      :aria-pressed="expanded">
-      <div class="level-left">
+      >
         <h4 class="level-item is-size-4">{{ title }}</h4>
-      </div>
+    </summary>
 
-      <div class="level-right">
-        <p v-if="expanded" class="level-item">hide</p>
-        <p v-else class="level-item">show</p>
-      </div>
-    </header>
-
-    <div v-show="expanded" class="px-3 py-3 has-background-white-ter is-size-6">
+    <div class="px-3 py-3 has-background-white-ter is-size-6">
       <slot></slot>
     </div>
-  </li>
+  </details>
 </template>
 
 <script>
@@ -60,6 +52,7 @@ export default {
   computed: {
     classObject: function () {
       return {
+        'open': this.expanded,
         'has-background-grey-lighter': !this.expanded,
         'has-background-primary': this.expanded,
         'active-hover': this.activeHover

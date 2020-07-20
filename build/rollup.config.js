9e7f9d76ae62736fa8c509ca0da7b724bebacad1
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-07-20 15:06:58 -0400

    fix: add global vue

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -55,7 +55,7 @@ const baseConfig = {
 const external = [
   // list external dependencies, exactly the way it is written in the import statement.
   // eg. 'jquery'
-  // 'vue'
+  'vue'
 ];
 
 // UMD/IIFE shared settings: output.globals
@@ -63,7 +63,7 @@ const external = [
 const globals = {
   // Provide global variable names to replace your external imports
   // eg. jquery: '$'
-  // vue: 'Vue'
+  vue: 'Vue'
 };
 
 // Customize configs for individual targets
